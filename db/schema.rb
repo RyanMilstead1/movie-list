41f1b1808533c16993b2b636e06108092fc538c8
Ryan Milstead <ryanmilstead1@gmail.com>
2018-03-08 16:01:41 -0500

    Add login/out and remove devise recoverable module

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20180308164101) do
   end
 
   create_table "ratings", force: :cascade do |t|
-    t.integer "rating"
+    t.integer "score"
     t.bigint "user_id"
     t.bigint "movie_id"
     t.datetime "created_at", null: false
@@ -52,8 +52,6 @@ ActiveRecord::Schema.define(version: 20180308164101) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
@@ -63,7 +61,6 @@ ActiveRecord::Schema.define(version: 20180308164101) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
