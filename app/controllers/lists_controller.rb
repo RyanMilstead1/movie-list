8864706f626b98f6add50a76a462f226662e4c80
Ryan Milstead <ryanmilstead1@gmail.com>
2018-03-12 02:09:53 -0400

    Work on list index and show views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
   # GET /lists
   # GET /lists.json
   def index
-    @lists = List.all
+    @lists = List.where(user: current_user)
   end
 
   # GET /lists/1
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,13 @@ class List < ApplicationRecord
 
   belongs_to :user
   has_and_belongs_to_many :movies
+
+  def avg_rating(user_id)
+    ratings = []
+    movies.each do |movie|
+      potential_rating = movie.ratings.where(user_id: user_id)
+      ratings << potential_rating[0].score unless potential_rating.empty?
+    end
+    (ratings.inject { |sum, el| sum + el }.to_f / ratings.size).round(2)
+  end
 end
